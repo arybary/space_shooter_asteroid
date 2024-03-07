e81e856084581dfc66fe178c82c1b1c07bff1fea
arybary <l3fljl5ldydy@gmail.com>
2024-03-07 14:32:39 +0200

    mobile  control

--- src/utils/PlayerController.ts
+++ src/utils/PlayerController.ts
@@ -1,6 +1,6 @@
+import { FederatedPointerEvent } from "pixi.js";
 import { Ship } from "../game/Ship";
 import { GameScene } from "../scenes/GameScene";
-
 
 interface IPlayerController {
     game: GameScene;
@@ -9,16 +9,22 @@ interface IPlayerController {
 
 export class PlayerController {
     private player: Ship;
+    private game: GameScene;
 
-    constructor({ player }: IPlayerController) {
+    constructor({ game, player }: IPlayerController) {
         this.player = player;
-        this.addEventListeners();
-    }
+      this.game = game;
+      this.addEventListeners();
+  }
 
     private addEventListeners(): void {
-        window.addEventListener("keydown", this.handleKeyDown);
-        window.addEventListener("keyup", this.handleKeyUp);
-    }
+      this.game.eventMode = "dynamic";
+      this.game.on("pointerdown", this.handlePlayerStartMove);
+      this.game.on("pointermove", this.handlePlayerKeepMove);
+      this.game.on("pointerup", this.handlePlayerStopMove);
+      window.addEventListener("keydown", this.handleKeyDown);
+      window.addEventListener("keyup", this.handleKeyUp);
+  }
 
     private handleKeyDown = (e: KeyboardEvent): void => {
         switch (e.code) {
@@ -28,18 +34,18 @@ export class PlayerController {
             case "ShiftLeft":
                 this.player.state.shoot = true;
 
-              break;
-          case "KeyA":
-          case "ArrowLeft":
-              this.player.state.movingLeft = true;
-              this.player.state.movingRight = false;
-              break;
-          case "KeyD":
-          case "ArrowRight":
-              this.player.state.movingLeft = false;
-              this.player.state.movingRight = true;
-              break;
-      }
+            break;
+        case "KeyA":
+        case "ArrowLeft":
+            this.player.state.movingLeft = true;
+            this.player.state.movingRight = false;
+            break;
+        case "KeyD":
+        case "ArrowRight":
+            this.player.state.movingLeft = false;
+            this.player.state.movingRight = true;
+            break;
+    }
   };
 
     private handleKeyUp = (e: KeyboardEvent): void => {
@@ -54,12 +60,45 @@ export class PlayerController {
             case "ArrowLeft":
                 this.player.state.movingLeft = false;
 
-              break;
-          case "KeyD":
-          case "ArrowRight":
-              this.player.state.movingLeft = false;
-              this.player.state.movingRight = false;
-              break;
-      }
+            break;
+        case "KeyD":
+        case "ArrowRight":
+            this.player.state.movingLeft = false;
+            this.player.state.movingRight = false;
+            break;
+    }
   };
+    private handlePlayerMove(
+        pressed: boolean | undefined,
+        e: FederatedPointerEvent
+    ): void {
+        const point = this.game.toLocal(e.global);
+        console.log(pressed, point.x, point.y);
+        if (pressed && point.x > this.player.x) {
+            this.player.state.movingRight = true;
+        }
+        if (pressed && point.x < this.player.x) {
+            this.player.state.movingLeft = true;
+        }
+        if (pressed && point.y < this.game.height - this.player.height) {
+            this.player.state.shoot = true;
+        }
+        if (!pressed) {
+            this.player.state.movingLeft = false;
+            this.player.state.movingRight = false;
+            this.player.state.shoot = false;
+        }
+    }
+
+    private handlePlayerStartMove = (e: FederatedPointerEvent): void => {
+        this.handlePlayerMove(true, e);
+    };
+
+    private handlePlayerKeepMove = (e: FederatedPointerEvent): void => {
+        this.handlePlayerMove(undefined, e);
+    };
+
+    private handlePlayerStopMove = (e: FederatedPointerEvent): void => {
+        this.handlePlayerMove(false, e);
+    };
 }
